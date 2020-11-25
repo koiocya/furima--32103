cbbddf655c25bfaa62abf6af1a05e9686a0e0199
koiocya <73815463+koiocya@users.noreply.github.com>
2020-11-25 19:05:53 +0900

    テストコード、itemコントローラーの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,8 +28,18 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Category Select')
         end
+        it 'カテゴリーの情報(id:1)だと保存できない' do
+          @item.category_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Category Select')
+        end
         it '商品の状態についての情報が必須であること' do
           @item.sales_status = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Sales status Select')
+        end
+        it '商品の状態についての情報(id:1)だと保存できない' do
+          @item.sales_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Sales status Select')
         end
@@ -38,8 +48,18 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Shipping fee status Select')
         end
+        it '配送料の負担についての情報(id:1)だと保存できない' do
+          @item.shipping_fee_status_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Shipping fee status Select')
+        end
         it '発送元の地域についての情報が必須であること' do
           @item.prefecture = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Prefecture Select')
+        end
+        it '発送元の地域についての情報(id:1)だと保存できない' do
+          @item.prefecture_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Prefecture Select')
         end
@@ -48,13 +68,22 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Scheduled delivery Select')
         end
+        it '発送までの日数についての情報(id:1)だと保存できない' do
+          @item.scheduled_delivery_id = 1
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Scheduled delivery Select')
+        end
         it '価格についての情報が必須であること' do
           @item.price = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Price can't be blank")
         end
-        it '価格の範囲が、¥300~¥9,999,999の間であること' do
+        it '価格の範囲が、¥300以上であること' do
           @item.price = 299
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price Out of setting range')
+        end
+        it '価格の範囲が、¥10_000_000未満であること' do
           @item.price = 10_000_000
           @item.valid?
           expect(@item.errors.full_messages).to include('Price Out of setting range')
