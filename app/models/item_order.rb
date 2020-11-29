dae4feb6adf89e16a1819e19d7b1fae7e46b878a
koiocya <73815463+koiocya@users.noreply.github.com>
2020-11-29 22:58:55 +0900

    Revert "pay.jp実装"
    This reverts commit 091776b333a24c2a8dca94d2b16bac54e618c8a3.

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,6 +1,6 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :post_code, :city, :house_number, :building_name, :phone_number,:order_id, :prefecture_id, :user_id, :item_id, :token
+  attr_accessor :post_code, :city, :house_number, :building_name, :phone_number,:order_id, :prefecture_id, :user_id, :item_id
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
@@ -8,7 +8,8 @@ class ItemOrder
     validates :city
     validates :house_number
     validates :phone_number
-    validates :token
+    # validates :card_number, :card_exp_month, :card_exp_year, :ard_cvc
+    # validates :authenticity_token
   end
   validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "number Input only number"}
    #郵便番号に関するバリデーション
@@ -16,9 +17,12 @@ class ItemOrder
    #都道府県に関するバリデーション
    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
   def save
+   
     # 購入の情報を保存
-    order = Order.create(user_id: user_id, item_id: item_id)
+    Order.create(user_id: user_id, item_id: item_id)
     # 住所の情報を保存
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, post_code: post_code, prefecture_id: prefecture_id, order_id: order_id)
+  
+    
   end
 end
